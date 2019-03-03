bda8056027eec9dfb85474fb483249770c8a6e2c
JGrndn <jerome.grondin.fr@gmail.com>
2019-03-03 22:48:06 +0100

    get all data

--- report.js
+++ report.js
@@ -1,11 +1,14 @@
 (function () {
   'use strict';
-
-  var wId = 2070626;
+  
+  const userAgent = 'erwin api test';
+  const url = 'https://www.toggl.com/api/v8';
+  const reportUrl = 'https://toggl.com/reports/api/v2/details';
+  const wId = 2070626;
   var outputFolder = './output/';
 
 
-  var argv = require('minimist')(process.argv.slice(2), {
+  const argv = require('minimist')(process.argv.slice(2), {
     string: [ 'user', 'output' ],
     boolean: [ 'version', 'help' ],
     alias: { h:'help', v:'version', u:'user', o:'output' },
@@ -14,7 +17,7 @@
   });
 
   function outputUsage() {
-    var o = [];
+    let o = [];
     o.push('** erwin Professional Services Report manager **');
     o.push('Usage : ');
     o.push('--user|-u TOKEN \t\tMandatory. Set the user token used to retrieve data.');
@@ -24,23 +27,18 @@
     console.info(o.join('\n'));
   }
 
-  if (argv.h){
+  if (argv.h || !argv.u){
     outputUsage();
     return;
   }
 
   if (argv.v){
-    var pjson = require('./package.json');
+    const pjson = require('./package.json');
     console.info(pjson.version);
     return;
   }
 
-  if (!argv.u){
-    outputUsage();
-    return;
-  }  
-
-  var opts = {
+  const opts = {
     auth: {
       user: argv.u,
       pass: 'api_token'
@@ -53,37 +51,72 @@
 
   outputFolder = (argv.o) ? argv.o : outputFolder;
 
+  const rp = require('request-promise-native');
+  const wait = () => new Promise(resolve => setTimeout(resolve, 1000)); // toggl api limitation is set to 1 request per second
 
-  var rp = require('request-promise');
-  var url = 'https://www.toggl.com/api/v8';
-
-
-  const wait = () => new Promise(resolve => setTimeout(resolve, 1000)); // toggl api limitation is set to 1 request / second
-
-  function getProjectUsers(projects){
-    //projects = projects.filter((v,i)=> i<3);
-    var userIdsByProjectId = {};
-    var promises = projects.reduce((chain, p) =>
-      chain
+  function getProjectUsers(json){
+    let userIdsByProjectId = {};
+    let promises = json.projects.reduce((chain, p) => chain
       .then(() => rp.get(url + '/projects/' + p.id + '/project_users', opts).then(data => {
         if (Array.isArray(data)) userIdsByProjectId[p.id] = data.map(u=> u.uid)
       }))
-    , Promise.resolve());
-    return promises.then(() => userIdsByProjectId);
+    , Promise.resolve(wait));
+    return promises.then(() => {
+      json.usersByProject = userIdsByProjectId;
+      return json;
+    });
   }
 
-  rp.get(url + '/workspaces/'+ wId + '/projects?active=both', opts)
-  .then(projects => getProjectUsers(projects))
-  .then(userIdsByProjectId => {
-    console.log('hello');
+  function getDetailedReport(json, pageNb){
+    let qs = {
+      user_agent: userAgent,
+      workspace_id: wId,
+      order_field: 'date',
+      page: (pageNb) ? pageNb : 1,
+      since : new Date().getFullYear()+'-01-01',
+      until : new Date().getFullYear()+'-12-31',
+    }
+    let options = Object.assign({}, opts);
+    options = Object.assign(options, {qs : qs});
+    
+    return rp.get(reportUrl, options).then(res => {
+      if (!json.timeEntries || !Array.isArray(json.timeEntries)){
+        json.timeEntries = [];
+      }
+      json.timeEntries = json.timeEntries.concat(res.data);
+      if (res.total_count > res.per_page && res.data.length === res.per_page) {
+        return getDetailedReport(json, qs.page+1);
+      }
+      return json;
+    });
+  }
+
+  Promise.all([
+    rp.get(url + '/workspaces/' + wId + '/clients', opts), // get workspace clients
+    rp.get(url + '/workspaces/' + wId + '/users', opts), // get workspace users
+    rp.get(url + '/workspaces/' + wId + '/projects?active=both', opts), // get workspace projects (active & archive)
+    rp.get(url + '/workspaces/' + wId + '/tasks', opts) // get workspace tasks
+  ])
+  .then(res => {
+    return {
+      clients : res[0],
+      users : res[1],
+      projects : res[2],
+      tasks : res[3] 
+    }
   })
-  .catch(function(err){
+  .then(json => {
+    json.projects = json.projects.filter((v,i)=> i<3); // to be removed
+    return getProjectUsers(json)
+  })
+  .then(json => getDetailedReport(json))
+  .then(json => {
+    // at this point, "json" contains all required data to be updated to csv file
+    console.log('Time to transform data !');
+  })
+  .catch(err =>{
     console.error(err);
   });
 
-  // promise to get projects & users
-  // promise to get tasks
-  // promise to get timeentries
-  // promise to get clients
 
 }());
